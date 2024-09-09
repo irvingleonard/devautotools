8e8dbaa38de48b575f09e987685fed7d83a154cb
Irving Leonard <irvingleonard@gmail.com>
2024-09-09 08:30:14 -0400

    Initial commit

---
+++ devautotools/__main__.py
@@ -0,0 +1,12 @@
+#!python
+"""Developers automated tools
+Several tools to automate development related tasks.
+
+This is the executable script
+"""
+
+import simplifiedapp
+
+from . import *
+
+simplifiedapp.main()
