fd9b63abe30f49e8e54374d181359a5b9289054d
Irving Leonard <irvingleonard@gmail.com>
2025-06-25 13:54:57 -0400

    deploy_local_venv fully expose venv creation and pip install options

--- devautotools/_venv.py
+++ devautotools/_venv.py
@@ -12,12 +12,12 @@ from ._venvctrl import VirtualEnvironmentManager
 
 LOGGER = getLogger(__name__)
 
-def deploy_local_venv(system_site_packages=False):
+def deploy_local_venv(*, env_create_options={}, pip_install_options={}):
 	"""Deploy a local virtual environment
 	Based on the current working directory, creates a python3 virtual environment (of the default python 3 on the system) on "./venv/" and populates it with the dependencies described on the "./pyproject.toml" file.
 	"""
 	
-	virtual_env = VirtualEnvironmentManager(overwrite=True, system_site_packages=system_site_packages)
+	virtual_env = VirtualEnvironmentManager('venv', True, **env_create_options)
 	
 	pyproject_toml_path = Path.cwd() / 'pyproject.toml'
 	
@@ -29,15 +29,15 @@ def deploy_local_venv(system_site_packages=False):
 
 	if ('build-system' in pyproject_toml) and ('requires' in pyproject_toml['build-system']):
 		LOGGER.info('Installing build related modules')
-		virtual_env.install(*pyproject_toml['build-system']['requires'])
+		virtual_env.install(*pyproject_toml['build-system']['requires'], **pip_install_options)
 	
 	if ('project' in pyproject_toml) and ('dependencies' in pyproject_toml['project']):
 		LOGGER.info('Installing dependencies')
-		virtual_env.install(*pyproject_toml['project']['dependencies'])
+		virtual_env.install(*pyproject_toml['project']['dependencies'], **pip_install_options)
 	
 	if ('project' in pyproject_toml) and ('optional-dependencies' in pyproject_toml['project']):
 		for section, modules in pyproject_toml['project']['optional-dependencies'].items():
 			LOGGER.info('Installing optional dependencies: %s', section)
-			virtual_env.install(*modules)
+			virtual_env.install(*modules, **pip_install_options)
 	
 	return virtual_env, pyproject_toml
