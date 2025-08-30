756aa0a330945d0532d1f8d933a34dbe3fa64580
Irving Leonard <irvingleonard@gmail.com>
2025-08-29 21:41:24 -0400

    Unneeded intermediate var

--- devautotools/_django.py
+++ devautotools/_django.py
@@ -103,8 +103,7 @@ def django_common_settings(settings_globals, parent_callables=None):
 	database_settings, database_options = {}, {}
 	for key in django_settings['ENVIRONMENTAL_SETTINGS_KEYS']:
 		if key[:24] == 'DJANGO_DATABASE_OPTIONS_':
-			setting_file_path = path_for_setting(django_settings=django_settings, env_var_name=key, lowercase=True)
-			database_options[key[24:]] = setting_file_path
+			database_options[key[24:]] = path_for_setting(django_settings=django_settings, env_var_name=key, lowercase=True)
 		elif key[:16] == 'DJANGO_DATABASE_':
 			database_settings[key[16:]] = django_settings['ENVIRONMENTAL_SETTINGS'][key]
 	if database_settings:
